4b5a0c81f42f0502d6d88a652da0f6cace3fb783
Roman Kholopov <roma.kholopov@gmail.com>
2022-09-02 18:15:41 +0300

    feat: create basic project structure

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
+import React, { StrictMode } from "react";
+import  ReactDOM  from "react-dom/client";
+import App from "./components/app/app";
 
+const root = ReactDOM.createRoot(document.getElementById('root'));
 
-console.log('hi')
+root.render(
+	<StrictMode>
+		<App />
+	</StrictMode>
+)
