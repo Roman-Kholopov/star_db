325db0286b869ca07e7a2688af8b8592c1abe386
Roman Kholopov <roma.kholopov@gmail.com>
2022-09-14 19:22:12 +0300

    feat: refactor app.js, create /pages/...

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,18 +1,11 @@
 import React, { Component } from 'react';
 import Header from '../header';
 import RandomPlanet from '../random-planet';
-import ErrorIndicator from '../error-indicator';
 import SwapiService from '../../services/swapi-service';
 import DummySwapiService from '../../services/dummy-swapi-service';
 import ErrorBoundry from '../error-boundry';
-import {
-	PersonDetails,
-    PlanetDetails,
-    StarshipDetails,
-    PersonList,
-    PlanetList,
-    StarchipList
-} from "../sw-components";
+import { PeoplePage, PlanetsPage, StarshipsPage } from '../pages';
+
 import { SwapiServiceProvider } from '../swapi-service-context';
 
 import './app.css';
@@ -20,31 +13,18 @@ import './app.css';
 export default class App extends Component {
 
 	state = {
-		showRandomPlanet: true,
-		hasError: false,
-		swapiService: new DummySwapiService()
+		swapiService: new SwapiService()
 	};
 
 	onServiceChange = () => {
 		this.setState(({swapiService}) => {
-			
 			const Servise = swapiService instanceof SwapiService ? DummySwapiService : SwapiService;
-
-			console.log(`switched to ${Servise.name}`);
 
 			return {
 				swapiService: new Servise(),
 			}
 		})
 	}
-
-	toggleRandomPlanet = () => {
-		this.setState((state) => {
-		return {
-			showRandomPlanet: !state.showRandomPlanet
-		}
-		});
-	};
 
 	onPersonSelected = (id) => {
 		this.setState({
@@ -61,41 +41,20 @@ export default class App extends Component {
 
   	render() {
 
-		if(this.state.hasError) {
-			return <ErrorIndicator/>
-		}
-
-		const { showRandomPlanet, selectedPerson } = this.state;
-
-		const planet = showRandomPlanet ?
-		<RandomPlanet/> :
-		null;
-
-		const {
-			getPerson,
-			getStarship,
-			getPersonImage,
-			getAllpeople,
-			getAllPlanets } = this.state.swapiService
-
 		return (
 			<ErrorBoundry>
 				<SwapiServiceProvider value={this.state.swapiService}>
 					<div className="stardb-app">
 						<Header onServiceChange={this.onServiceChange}/>
 
-						<PersonDetails itemId={11}/>
+						<RandomPlanet/>
 
-						<PlanetDetails itemId={5}/>
+						<PeoplePage />
 
-						<StarshipDetails itemId={9}/>
-			
-						<PersonList/>
+						<PlanetsPage />
 
-						<PlanetList/>
+						<StarshipsPage />
 
-						<StarchipList/>
-			
 					</div>
 				</SwapiServiceProvider>
 			</ErrorBoundry>
